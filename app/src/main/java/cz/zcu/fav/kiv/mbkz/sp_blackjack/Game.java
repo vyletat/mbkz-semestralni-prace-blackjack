d7713a09f7038b0e3c9aaf39564382996b39cfd4
Tomáš Vyleta <vyleta.tom@gmail.com>
2021-03-31 14:03:46 +0200

    Dealer logic

--- app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/Game.java
+++ app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/Game.java
@@ -2,37 +2,50 @@ package cz.zcu.fav.kiv.mbkz.sp_blackjack;
 
 import android.util.Log;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import cz.zcu.fav.kiv.mbkz.sp_blackjack.enums.Rank;
-import cz.zcu.fav.kiv.mbkz.sp_blackjack.enums.Suit;
 
 public class Game {
     private final int GOAL = 21;
 
-    private int bankSum;
-    private int counterRound;                   // current round 0-4
-    private CardDeck dealerDeck;
-    private CardDeck handDeck;
-    private int dealerScore = 0;
-    private int handScore = 0;
+    // Money
+    private int bankSum, bet;
+
+    // Cards
+    private CardDeck dealerDeck, handDeck;
     List<Card> dealersFive, handsFive;
+
+    public int getDealerRound() {
+        return dealerRound;
+    }
+
+    // Counters
+    private int handRound, dealerRound;                   // current round 0-4
+
+    // Score
+    private int dealerScore, handScore = 0;
+
 
     public Game(int bankSum, int cardsInDeck) {
         this.bankSum = bankSum;
-        this.counterRound = 0;
+        this.handRound = 0;
         this.dealerDeck = new CardDeck();
         this.handDeck = new CardDeck();
     }
 
+    /**
+     * FOR HAND
+     */
     public void next() {
-        Log.v("Next", "Round index: " + this.counterRound);
+        Log.v("Next", "Round index: " + this.handRound);
 
-        this.dealerScore += this.dealersFive.get(this.getCounterRound()).value;
-        this.handScore += this.handsFive.get(this.getCounterRound()).value;
+        // this.dealerScore += this.dealersFive.get(this.getCounterRound()).value;
+        this.handScore += this.handsFive.get(this.getHandRound()).value;
+        this.handRound++;
+    }
 
-        this.counterRound++;
+    public void nextDealer() {
+        this.dealerRound++;
+        this.dealerScore += this.dealersFive.get(this.getDealerRound()).value;
     }
 
     private void stop() {
@@ -40,23 +53,27 @@ public class Game {
     }
 
     public void nextRound() {
-        this.counterRound = 0;
+        // init zero
+        this.dealerRound = 0;
+        this.handRound = 0;
         this.dealerScore = 0;
         this.handScore = 0;
 
+        // get next five card from deck
         this.dealersFive = this.dealerDeck.getNextFive();
         this.handsFive = this.handDeck.getNextFive();
 
-        this.dealerScore += this.dealersFive.get(this.getCounterRound()).value;
-        this.handScore += this.handsFive.get(this.getCounterRound()).value;
+        // add first card score do total score of each site
+        this.dealerScore += this.dealersFive.get(this.getHandRound()).value;
+        this.handScore += this.handsFive.get(this.getHandRound()).value;
     }
 
     public int getBankSum() {
         return bankSum;
     }
 
-    public int getCounterRound() {
-        return counterRound;
+    public int getHandRound() {
+        return handRound;
     }
 
     public int getDealerScore() {
--- app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/GameActivity.java
+++ app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/GameActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,14 +12,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
-
 public class GameActivity extends AppCompatActivity {
     ImageView dealer_card_first, dealer_card_second, dealer_card_third, dealer_card_fourth, dealer_card_fifth;
     ImageView hand_card_first, hand_card_second, hand_card_third, hand_card_fourth, hand_card_fifth;
     TextView bank, dealer_score, hand_score;
+    Button hit, stand, place_bet, surrender;
     Game game;
-    Button hit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +46,14 @@ public class GameActivity extends AppCompatActivity {
 
         // Buttons
         this.hit = (Button) findViewById(R.id.button_hit);
+        this.stand = (Button) findViewById(R.id.button_stand);
 
         this.game = new Game(1000, 52);
         this.reset();
     }
 
     public void hit(View view) {
-        int index = this.game.getCounterRound();
+        int index = this.game.getHandRound();
 
         Card dealerCard = this.game.getDealersFive().get(index);
         Card handCard = this.game.getHandsFive().get(index);
@@ -69,27 +68,27 @@ public class GameActivity extends AppCompatActivity {
                 Log.v("Next", "fifth");
                 break;
             case 1:
-                dealer_card_fourth.setImageResource(dealerCardImage);
+                //dealer_card_fourth.setImageResource(dealerCardImage);
                 hand_card_fourth.setImageResource(handCardImage);
                 Log.v("Next", "fourth");
                 break;
             case 2:
-                dealer_card_third.setImageResource(dealerCardImage);
+                //dealer_card_third.setImageResource(dealerCardImage);
                 hand_card_third.setImageResource(handCardImage);
                 Log.v("Next", "third");
                 break;
             case 3:
-                dealer_card_second.setImageResource(dealerCardImage);
+                //dealer_card_second.setImageResource(dealerCardImage);
                 hand_card_second.setImageResource(handCardImage);
                 Log.v("Next", "second");
                 break;
             case 4:
-                dealer_card_first.setImageResource(dealerCardImage);
+                //dealer_card_first.setImageResource(dealerCardImage);
                 hand_card_first.setImageResource(handCardImage);
                 Log.v("Next", "first");
                 break;
             default:
-                dealer_card_fifth.setImageResource(dealerCardImage);
+                //dealer_card_fifth.setImageResource(dealerCardImage);
                 hand_card_fifth.setImageResource(handCardImage);
                 Log.v("Next", "fifth");
                 break;
@@ -138,17 +137,295 @@ public class GameActivity extends AppCompatActivity {
             AlertDialog dialog = builder.create();
             dialog.show();
         }
+    }
 
+    public void stand(View view) {
+        final int MAX_CARD_SCORE = 10;
+        final int GOAL = 21;
 
+        if (this.game.getDealerScore() + MAX_CARD_SCORE <= GOAL) {
+            this.game.nextDealer();
 
+            int index = this.game.getDealerRound();
+            Card dealerCard = this.game.getDealersFive().get(index);
+            int dealerCardImage = getResources().getIdentifier(dealerCard.resource, "drawable", getPackageName());
+
+            switch (index) {
+                case 0:
+                    dealer_card_fifth.setImageResource(dealerCardImage);
+                    //hand_card_fifth.setImageResource(handCardImage);
+                    Log.v("Next", "fifth");
+                    break;
+                case 1:
+                    dealer_card_fourth.setImageResource(dealerCardImage);
+                    //hand_card_fourth.setImageResource(handCardImage);
+                    Log.v("Next", "fourth");
+                    break;
+                case 2:
+                    dealer_card_third.setImageResource(dealerCardImage);
+                    //hand_card_third.setImageResource(handCardImage);
+                    Log.v("Next", "third");
+                    break;
+                case 3:
+                    dealer_card_second.setImageResource(dealerCardImage);
+                    //hand_card_second.setImageResource(handCardImage);
+                    Log.v("Next", "second");
+                    break;
+                case 4:
+                    dealer_card_first.setImageResource(dealerCardImage);
+                    //hand_card_first.setImageResource(handCardImage);
+                    Log.v("Next", "first");
+                    break;
+                default:
+                    dealer_card_fifth.setImageResource(dealerCardImage);
+                    //hand_card_fifth.setImageResource(handCardImage);
+                    Log.v("Next", "fifth");
+                    break;
+            }
+
+            this.stand.performClick();
+
+        } else {
+            int nextCard = this.probabilityNextCard();
+
+            if (this.game.getDealerScore() + nextCard <= GOAL) {
+                // HRAJE
+                this.game.nextDealer();
+
+                int index = this.game.getDealerRound();
+                Card dealerCard = this.game.getDealersFive().get(index);
+                int dealerCardImage = getResources().getIdentifier(dealerCard.resource, "drawable", getPackageName());
+
+                switch (index) {
+                    case 0:
+                        dealer_card_fifth.setImageResource(dealerCardImage);
+                        //hand_card_fifth.setImageResource(handCardImage);
+                        Log.v("Next", "fifth");
+                        break;
+                    case 1:
+                        dealer_card_fourth.setImageResource(dealerCardImage);
+                        //hand_card_fourth.setImageResource(handCardImage);
+                        Log.v("Next", "fourth");
+                        break;
+                    case 2:
+                        dealer_card_third.setImageResource(dealerCardImage);
+                        //hand_card_third.setImageResource(handCardImage);
+                        Log.v("Next", "third");
+                        break;
+                    case 3:
+                        dealer_card_second.setImageResource(dealerCardImage);
+                        //hand_card_second.setImageResource(handCardImage);
+                        Log.v("Next", "second");
+                        break;
+                    case 4:
+                        dealer_card_first.setImageResource(dealerCardImage);
+                        //hand_card_first.setImageResource(handCardImage);
+                        Log.v("Next", "first");
+                        break;
+                    default:
+                        dealer_card_fifth.setImageResource(dealerCardImage);
+                        //hand_card_fifth.setImageResource(handCardImage);
+                        Log.v("Next", "fifth");
+                        break;
+                }
+
+                this.stand.performClick();
+            } else {
+                // KONCI
+                Toast.makeText(this, "Konci!", Toast.LENGTH_SHORT).show();
+
+                // porovnat score
+                if (this.game.getDealerScore() == this.game.getGOAL()) {
+                    // LOSE
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(R.string.lose_header)
+                            .setTitle(R.string.lose_text);
+                    builder.setPositiveButton(R.string.next_round, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            reset();
+                        }
+                    });
+                    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else if (this.game.getDealerScore() > this.game.getGOAL()) {
+                    // WIN
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(R.string.win_header)
+                            .setTitle(R.string.win_text);
+                    builder.setPositiveButton(R.string.next_round, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            reset();
+                        }
+                    });
+                    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    int dealerDiff = this.game.getGOAL() - this.game.getDealerScore();
+                    int handDiff = this.game.getGOAL() - this.game.getHandScore();
+
+                    if (dealerDiff == handDiff) {
+                        // Plichta
+                    }
+                    else if (dealerDiff < handDiff) {
+                        // LOSE
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(R.string.lose_header)
+                                .setTitle(R.string.lose_text);
+                        builder.setPositiveButton(R.string.next_round, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                reset();
+                            }
+                        });
+                        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    } else {
+                        // WIN
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(R.string.win_header)
+                                .setTitle(R.string.win_text);
+                        builder.setPositiveButton(R.string.next_round, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                reset();
+                            }
+                        });
+                        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            }
+        }
     }
 
     private void initGame() {
         // Todo: Sebrat to z nastaveni
     }
 
-    private void reset() {
+    /**
+     *
+     * @return
+     */
+    private int probabilityNextCard() {
+        double random = Math.random();
+        final double CARDS = 52.0;      // cards total
+        final double NUMBER = 4.0;      // cards from one color
+        double oneNumber = NUMBER / CARDS;
+        int nextCard = 0;
 
+        // probabilities
+        double one = oneNumber,
+                two = 2 * oneNumber,
+                three = 3 * oneNumber,
+                four = 4 * oneNumber,
+                five = 5 * oneNumber,
+                six = 6 * oneNumber,
+                seven = 7 * oneNumber,
+                eight = 8 * oneNumber,
+                nine = 9 * oneNumber,
+                ten = (4 * NUMBER) / CARDS;
+
+        if (random <= one) {
+            nextCard = 1;
+        } else {
+            if (random <= two) {
+                nextCard = 2;
+            } else {
+                if (random <= three) {
+                    nextCard = 3;
+                } else {
+                    if (random <= four) {
+                        nextCard = 4;
+                    } else {
+                        if (random <= five) {
+                            nextCard = 5;
+                        } else {
+                            if (random <= six) {
+                                nextCard = 6;
+                            } else {
+                                if (random <= seven) {
+                                    nextCard = 7;
+                                } else {
+                                    if (random <= eight) {
+                                        nextCard = 8;
+                                    } else {
+                                        if (random <= nine) {
+                                            nextCard = 9;
+                                        } else {
+                                            nextCard = 10;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return nextCard;
+    }
+
+    private void nextDealer() {
+        this.game.nextDealer();
+
+        int index = this.game.getDealerScore();
+        Card dealerCard = this.game.getDealersFive().get(index);
+        int dealerCardImage = getResources().getIdentifier(dealerCard.resource, "drawable", getPackageName());
+
+        switch (index) {
+            case 0:
+                dealer_card_fifth.setImageResource(dealerCardImage);
+                //hand_card_fifth.setImageResource(handCardImage);
+                Log.v("Next", "fifth");
+                break;
+            case 1:
+                dealer_card_fourth.setImageResource(dealerCardImage);
+                //hand_card_fourth.setImageResource(handCardImage);
+                Log.v("Next", "fourth");
+                break;
+            case 2:
+                dealer_card_third.setImageResource(dealerCardImage);
+                //hand_card_third.setImageResource(handCardImage);
+                Log.v("Next", "third");
+                break;
+            case 3:
+                dealer_card_second.setImageResource(dealerCardImage);
+                //hand_card_second.setImageResource(handCardImage);
+                Log.v("Next", "second");
+                break;
+            case 4:
+                dealer_card_first.setImageResource(dealerCardImage);
+                //hand_card_first.setImageResource(handCardImage);
+                Log.v("Next", "first");
+                break;
+            default:
+                dealer_card_fifth.setImageResource(dealerCardImage);
+                //hand_card_fifth.setImageResource(handCardImage);
+                Log.v("Next", "fifth");
+                break;
+        }
+    }
+
+    private void reset() {
         int cardBack = getResources().getIdentifier("card_back_blue", "drawable", getPackageName());
 
         dealer_card_first.setImageResource(cardBack);
