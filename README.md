ea2cee29cbec52a87f3a0181cc926ce537ea8963
Tomáš Vyleta <vyleta.tom@gmail.com>
2021-04-04 14:56:41 +0200

    Night mode and setting, new activity

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # mbkz-sem-blackjack
- Semestralni prace na předmět KIV/MBKZ. Android aplikace - karetní hra Blackjack.
+ Semestralni prace na předmět KIV/MBKZ. Android aplikace - karetní hra Blackjack (Vlastní pravidla).
  
  # Zadání
  
+ 
  # Informace
+ 
  
  # Pravidla
  
+ 
  # TODO
- * 
+ * ReferenceActivity - udělat activity, ve které budou odkazy na autora
+    * Webový odkaz
+ * Sepsat pravidla hry
+ * na Game over nebo vzdání hry, zapsat score - Scoreboard
+    * Vymyset systém bodování
+* Night mode - změna během runu aplikace
+* Ošetření dealera pro i > 5
+* Settings - pouze Number input
+* ikona aplikace
+* Pozadí v MENU
+    * možná zelené pozadí ve hře
  
--- app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/MainActivity.java
+++ app/src/main/java/cz/zcu/fav/kiv/mbkz/sp_blackjack/MainActivity.java
@@ -1,9 +1,13 @@
 package cz.zcu.fav.kiv.mbkz.sp_blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean nightMode = prefs.getBoolean("switch_preference_dark_mode", false);
+        Log.v("Settings", nightMode.toString());
+
+        // Nigh mode
+        if (nightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
         setContentView(R.layout.activity_main);
     }
 
